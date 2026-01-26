b257a85b7981f49585a5bd30b3827f06ef33eb0c
jloptes-cmyk <jloptes@gmail.com>
2026-01-26 22:38:40 +0100

    Set correct Apps Script scores URL

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.SCORES_API_URL = "https://script.google.com/macros/s/AKfycbyVMOOOhSE_a8EoR67_dZKY2V_mhNpPDggZJ_Hx6LGlKa1QIE_FH1M0uXhBlK9UxSpQ3w/exec";
+window.SCORES_API_URL="https://script.google.com/macros/s/AKfycbyVMOOOhSE_a8EoR67_dZKY2V_mhNpPDggZJ_Hx6LGlKa1QIE_FH1M0uXhBlK9UxSpQ3w/exec";
